8b2fe70aee3c1c01d12676dd9d6623a8fb88027a
junhyoungkim <kkjn0131@naver.com>
2024-02-26 15:14:52 +0900

    Study React - Chap02 > Element와 렌더링

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import MyButton from "./chap01/MyButton";
-import DrinkList from "./chap01/DrinkList";
+// import MyButton from "./chap01/MyButton";
+// import DrinkList from "./chap01/DrinkList";
+import Clock from "./chap02/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<MyButton name="user" />
-		<DrinkList />
-		{/* <App /> */}
-	</React.StrictMode>
-);
+
+setInterval(() => {
+	root.render(
+		<React.StrictMode>
+			<Clock />
+		</React.StrictMode>
+	);
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
